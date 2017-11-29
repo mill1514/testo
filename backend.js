a7a2fa6852523d00a14e01ac25d8775d985edb0b
Ryan Miller <mill1514@purdue.edu>
2017-11-29 17:48:32 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -8,16 +8,19 @@
     storageBucket: "boiler-canvas.appspot.com",
     messagingSenderId: "985213101820"
   };
+
   firebase.initializeApp(config);
  
   var database = firebase.database();
   var x_max = 400;
   var y_max = 440;
 	
+  /* Gets the entire canvas data in string form */
   function getCanvas() {  
 	  return firebase.database().ref("map/").once('value');
   }
 
+  /* Sets the entire canvas */
   function setCanvas(canv) {
 	  
 	  /* Set the canvas as the given string */
@@ -25,11 +28,12 @@
 		pixels: canv
   	});
   }
-	
+
+  /* Sets a pixel of the canvas */
   function setPixel(x, y, val) {
 	  
 	  /* Get index of pixel to be changed */
-  var index = (x_max * y) + x - 1;
+        var index = (x_max * y) + x - 1;
 	  
 	  /* Get current canvas */
 	var currCanv = getCanvas();
