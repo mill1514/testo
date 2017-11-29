a40f2ce342e0ab54d17e9b72ce69c589f3c43c70
Ryan Miller <mill1514@purdue.edu>
2017-11-29 18:47:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+while (true) {
+  setTimeout(updateCanvas(), 5000);
+}
 
+/* Pulls from Firebase and updates javscript canvas element */
+function updateCanvas () {
+  
+  console.log("UPDATING");
+  
+  /* pull data */
+  var canv = getCanvas();
+  
+  /* put into canvas */
+  colorCanvas(canv);
+}
